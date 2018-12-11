79c4c02d32bf9e8254c98e7d05c46422f08e2806
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-11 08:50:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+const BoardMember = class BoardMember extends Ceo {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+}
+
+
+veto() {
+  return console.log('No, I must disagree');
+}
+
+
+  // BoardMember.prototype.veto = function() {
+  //   return 'No, I must disagree'
+  // };
+  // BoardMember.prototype.approve = function() {
+  //   return 'You can do that!'
+  // };
+  // BoardMember.prototype.doCharity = function() {
+  //   return "I like to help people."
+  // };
+  // BoardMember.prototype.releasePressStatement = function() {
+  //   return "You will see great things from Scuber."
+  // };
+  // BoardMember.prototype.sayHi = function() {
+  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  // };
+  // }
+  
+  const Ceo = class Ceo {
+    constructor(name, training, homeState) {
+      this.name = name;
+      this.training = training;
+      this.homeState = homeState;
+      
+hireEmployee() {
+  return "Welcome aboard"
+}
+    }
+  } {
